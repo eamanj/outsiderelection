4ba7ab1bfc0b5817e8c1d82b7de286afb5be3701
Eaman Jahani <eamanj@gmail.com>
2016-06-23 15:25:01 -0500

    update words to exclude

--- src/word_clouds.py
+++ src/word_clouds.py
@@ -53,7 +53,7 @@ def main():
     # Generate a word cloud image
     wordcloud = WordCloud(width=1000, height=600, stopwords=exclude).generate(text)
     wordcounts = WordCloud(stopwords=exclude).process_text(text)
-    most_common = print_word_counts(candidate, wordcounts, 20)
+    most_common = print_word_counts(candidate, wordcounts, 30)
     most_common_words_of_all_candidates = most_common_words_of_all_candidates.union(most_common)
 
     # Display the generated image:
