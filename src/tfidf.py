56c65d8cf8a0143432d85ae8fb77d0e8bf869730
llvll0hsen <mohsen.jadidi@gmail.com>
2016-06-23 22:52:44 -0500

    add tfidf data

--- src/tfidf.py
+++ src/tfidf.py
@@ -7,6 +7,10 @@ from nltk.corpus import stopwords
 import os
 import argparse
 from sklearn.feature_extraction.text import TfidfVectorizer
+from collections import defaultdict
+import numpy as np
+
+import csv
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("vocabulary",
@@ -26,43 +30,73 @@ candidates = ['clinton', 'sanders', 'martin', 'ted_cruz', 'rubio', 'kasich', 'bu
 
 def main():
     # get the list of vocab for which to compute tfidf
-  with open(args.vocabulary) as f:
-      vocab = f.read().splitlines()
-  vocab = set(vocab)
+    with open(args.vocabulary) as f:
+        vocab = f.read().splitlines()
+    vocab = set(vocab)
 
-  # get list of stopwords
-  exclude = stopwords.words('english')
-  if args.custom_exclude:
-      with open(args.custom_exclude) as f:
-          extra_words_exclude = f.read().splitlines()
+    # get list of stopwords
+    exclude = stopwords.words('english')
+    if args.custom_exclude:
+        with open(args.custom_exclude) as f:
+            extra_words_exclude = f.read().splitlines()
     exclude.extend(extra_words_exclude)
 
-  exclude = list(set(exclude))
+    exclude = list(set(exclude))
 
-  vect = TfidfVectorizer(sublinear_tf=args.sublinear_tf,
-          max_df=args.max_df,
-          analyzer='word', 
-          stop_words=exclude,
-          vocabulary=vocab,
-          norm=args.norm)
+    vect = TfidfVectorizer(sublinear_tf=args.sublinear_tf,
+            max_df=args.max_df,
+            analyzer='word', 
+            stop_words=exclude,
+            vocabulary=vocab,
+            norm=args.norm)
 
   # generate corpus
-  corpus = list()
-  corpus_docs = list()
-  for candidate in candidates:
-      directory = os.path.join(os.path.pardir, 'data', candidate)
-    for speech_file in os.listdir(directory):
-        if speech_file == 'all_speeches.txt':
-            continue
-      # now we know this is a single speech file
-      input_file = os.path.join(directory, speech_file)
-      # Read the whole text.
-      text = open(input_file).read().lower()
-      corpus.append(text)
-      corpus_docs.append(candidate + '_' + speech_file)
+    corpus = list()
+    corpus_docs = list()
+    i = 0
+    author_ids = defaultdict(list)
+    for candidate in candidates:
+        directory = os.path.join(os.path.pardir, 'data', candidate)
+        for speech_file in os.listdir(directory):
+            if speech_file == 'all_speeches.txt':
+                continue
+            # now we know this is a single speech file
+            input_file = os.path.join(directory, speech_file)
+            # Read the whole text.
+            text = open(input_file).read().lower()
+            corpus.append(text)
+            corpus_docs.append(candidate + '_' + speech_file)
+            author_ids[candidate].append(i)
+            i+=1
+    
+    corpus_tf_idf = vect.fit_transform(corpus)
+    features = np.array(vect.get_feature_names())
+    a = corpus_tf_idf.todense()
+    
+    ind = np.argsort(a)[::-1]
+    ind = ind[:,:100]
+    cand_str = defaultdict(str)
+    #f = open(os.path.join(os.path.pardir,'data','tdidfs_top100.csv'),'wb')
+    #csvwriter = csv.writer(f)
+    #csvwriter.writerow(['candidate','text'])
+    for candidate,ids in author_ids.iteritems():
+        fpath = '../data/tfidf/{0}'.format(candidate)
+        #os.mkdir(fpath)
+        f = open('{0}/all_speeches.txt'.format(fpath),"wb")
+        words = []
+        ids = np.array(ids) #row ids for current candidate
+        temp = ind[ids,:]  #extract those rows
+        for i in xrange(temp.shape[0]): #for  speech i
+            w = features[temp[i,:]] #extract top 100 words
+            print type(w.tolist())
+            print w.tolist()[0]
+            words.extend(w.tolist()[0])
 
-
-  corpus_tf_idf = vect.fit_transform(corpus)
-
+        #print words
+        cand_str =  " ".join(words)
+        f.write(cand_str)
+        #csvwriter.writerow(cand_str)
+        f.close()
+    
 if __name__ == '__main__':
     main()
